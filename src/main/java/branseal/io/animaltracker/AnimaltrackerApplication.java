be2d7133f54a1687cacaaf488f8a03dfe23d8a73
BranSeal <sealbran@gmail.com>
2021-03-13 13:21:17 -0500

    Commit initial project files

---
+++ src/main/java/branseal/io/animaltracker/AnimaltrackerApplication.java
@@ -0,0 +1,13 @@
+package branseal.io.animaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaltrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaltrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/branseal/io/animaltracker/AnimaltrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package branseal.io.animaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaltrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
