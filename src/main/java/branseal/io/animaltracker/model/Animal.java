1f6e8d76c622d29e7afe155f840bb104742dba6e
Brandon Seal <sealbran@mail.com>
2021-03-30 23:30:04 -0400

    Cleaned up Animal/Log POJOs

--- src/main/java/branseal/io/animaltracker/model/Animal.java
+++ src/main/java/branseal/io/animaltracker/model/Animal.java
@@ -1,10 +1,7 @@
 package branseal.io.animaltracker.model;
 
-import branseal.io.animaltracker.util.IllegalAnimalLogException;
-
 import javax.persistence.*;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 @Entity(name = "Animal")
@@ -17,20 +14,13 @@ public class Animal {
     private @OneToMany List<Log> weightLogs;
     private @OneToMany List<Log> medicationLogs;
 
-    private static final int expectedFeedingLogSize = 3;
-    private static final int expectedMedicationLogSize = 2;
+    public Animal() {}
 
-    public Animal() {
-
-    }
-
-    public Animal( Long id,
-            String name,
+    public Animal(String name,
             String dob,
             List<Log> feedingLogs,
             List<Log> weightLogs,
             List<Log> medicationLogs) {
-        this.id = id;
         this.name = name;
         this.dob = dob;
         this.feedingLogs = feedingLogs;
@@ -86,26 +76,21 @@ public class Animal {
         this.medicationLogs = medicationLogs;
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Animal animal = (Animal) o;
-        return Objects.equals(id, animal.id) && Objects.equals(name, animal.name) && Objects.equals(dob, animal.dob);
+        return Objects.equals(id, animal.id)
+                && Objects.equals(name, animal.name)
+                && Objects.equals(dob, animal.dob)
+                && Objects.equals(feedingLogs,animal.feedingLogs)
+                && Objects.equals(weightLogs, animal.weightLogs)
+                && Objects.equals(medicationLogs, animal.medicationLogs);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, dob);
-    }
-
-    @Override
-    public String toString() {
-        return "Animal{" +
-                "id=" + id +
-                ", name='" + name + "'" +
-                ", dob=" + dob +
-                "}";
+        return Objects.hash(id, name, dob, feedingLogs, weightLogs, medicationLogs);
     }
 }
